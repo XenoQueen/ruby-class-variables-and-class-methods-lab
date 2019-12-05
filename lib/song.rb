8808d0a3d72531eb47f3af4610673a81346c3617
Maika Kailani <queenmab051@gmail.com>
2019-12-05 20:57:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,5 +11,6 @@ class Song
   end
   
   def self.genre_count
-    @@genres.inject(Hash.new(0)) { |total, i| total[i] +=1 ;total
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] +=1 ;total}
+  end
 end
